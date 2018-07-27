a13b867c7d49f968b97271d2a30cf4d914f0e01a
mqschwanda <mqschwanda@gmail.com>
2018-07-27 16:37:47 -0400

    ready for heroku

---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export const firebase = {
+  apiKey: 'AIzaSyAEN41LOwUvWk5GnuDe9aOiIk8wbXu8OHk',
+  authDomain: 'mqs-survey-app.firebaseapp.com',
+  databaseURL: 'https://mqs-survey-app.firebaseio.com',
+  projectId: 'mqs-survey-app',
+  storageBucket: 'mqs-survey-app.appspot.com',
+  messagingSenderId: '25821180437'
+};

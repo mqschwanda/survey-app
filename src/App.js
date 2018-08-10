c543dae4acc134f870d1c5615b5a7a441aad9c90
mqschwanda <mqschwanda@gmail.com>
2018-08-10 12:55:45 -0400

    add browser router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './modules/firestore';
 import React from 'react';
 import './App.css';
 // import 'bootstrap/dist/css/bootstrap.css';
-
+import { BrowserRouter } from 'react-router-dom';
 import { Router, Redux } from './ui/Providers';
 
 export const App = () => (
   <Redux>
-    <Router />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </Redux>
 );
 

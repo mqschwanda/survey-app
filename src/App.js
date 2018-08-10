2096cdeeba6a5e40058c51bcf5bd2b6052c8d91f
mqschwanda <mqschwanda@gmail.com>
2018-08-10 13:05:50 -0400

    remove wrong static json

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import './modules/firestore';
 import React from 'react';
 import './App.css';
 // import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom';
 import { Router, Redux } from './ui/Providers';
 
 export const App = () => (
   <Redux>
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <Router />
   </Redux>
 );
 
